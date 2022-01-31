1f4c26cf2122100b352832473b2a9598f43092f8
jinhyeok15 <ekfrhd97@gmail.com>
2022-02-01 01:52:39 +0900

    svelte-routing 사용

--- example-app/src/App.svelte
+++ example-app/src/App.svelte
@@ -1,38 +1,41 @@
 <script>
-	import Router, {location} from 'svelte-spa-router';
-	import Mydoc from './Mydoc.svelte';
-	import Search from './Search.svelte';
-	import Package from './Package.svelte';
-	import Home from './Home.svelte';
+	import {Router, links, Route} from 'svelte-routing';
+	import Mydoc from './mydoc/Mydoc.svelte';
+	import Search from './search/Search.svelte';
+	import Package from './package/Package.svelte';
+	import Home from './home/Home.svelte';
+	// components
+	import NavLink from './components/NavLink.svelte';
 
-	let routes = {
-		"/": Home,
-		"/mydoc": Mydoc,
-		"/search": Search,
-		"/package": Package
-	}
+	export let url = "";
 </script>
 
 <link href='https://fonts.googleapis.com/css?family=Baloo' rel='stylesheet'>
-<main>
-	<header>
-		<a href="/"><div class="knock"><b>KnocK</b></div></a>
-		<div class="nav">
-			<div class="bars">
-				<div class="mydoc bars_component"><a href="/#/mydoc">내 문서</a></div>
-				<div class="search bars_component"><a href="/#/search">검색</a></div>
-				<div class="package bars_component"><a href="/#/package">패키지</a></div>
+
+<Router url="{url}">
+	<main>
+		<header>
+			<a href="/"><div class="knock"><b>KnocK</b></div></a>
+			<div class="nav">
+				<div class="bars" use:links>
+					<div class="mydoc bars_component"><NavLink path="/mydoc" navName="내 문서"/></div>
+					<div class="search bars_component"><NavLink path="/search" navName="검색"/></div>
+					<div class="package bars_component"><NavLink path="/packages" navName="패키지"/></div>
+				</div>
 			</div>
+			<div class="loginbar">
+				<div class="login loginbar_component"><a href="/">로그인</a></div>
+				<div class="join loginbar_component"><a href="/">회원가입</a></div>
+			</div>
+		</header>
+		<div class="body">
+			<Route path="/" component="{Home}"/>
+			<Route path="mydoc" component="{Mydoc}"/>
+			<Route path="search" component="{Search}"/>
+			<Route path="packages" component="{Package}"/>
 		</div>
-		<div class="loginbar">
-			<div class="login loginbar_component"><a href="/">로그인</a></div>
-			<div class="join loginbar_component"><a href="/">회원가입</a></div>
-		</div>
-	</header>
-	<div class="body">
-		<Router {routes}/>
-	</div>
-</main>
+	</main>	
+</Router>
 
 <style>
 	/* test border */
@@ -92,12 +95,7 @@
 		height: 50px;
 		text-align: center;
 		display:table-cell;
-		vertical-align:middle; 
-	}
-	.bars_component a {
-		font-size: 17px;
-		font-weight: bold;
-		color: black;
+		vertical-align:middle;
 	}
 	.mydoc {left: -1px;}.search {left: 97.5px;}.package {left: 195px;}
 
@@ -120,6 +118,7 @@
 		color: black;
 	}
 	.login {width: 80px; height: 30px;} .join {left: 100px; width: 80px; height: 30px;}
+	
 	@media screen and (max-width:1440px) {
 		header{left:0%; margin-left:0%;}
 		.body{left:0%; margin-left:0%;}
---
+++ example-app/src/components/NavLink.svelte
@@ -0,0 +1,31 @@
+<script>
+	let linkStatus = { clicked: false };
+
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+    export let path;
+    export let navName;
+</script>
+
+{#if !linkStatus.clicked}
+	<a href={path} class="nav-link" id="unclicked-nav" on:click={toggle}>
+		{navName}
+    </a>
+{:else}
+	<a href={path} class="nav-link" id="clicked-nav" on:click={toggle}>
+		{navName}
+    </a>
+{/if}
+
+
+<style>
+    .nav-link {
+        font-size: 17px;
+		font-weight: bold;
+		color: black;
+    }
+    #clicked-nav {
+        text-decoration: underline;
+    }
+</style>
